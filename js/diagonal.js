e9dce7054a2ec32f52e833f3fcb071ded395cfe0
soriac <sbcristiansoria@gmail.com>
2018-11-02 15:30:33 -0300

    Agrego lógica de detección de matriz diagonalmente dominante. #3

---
+++ js/diagonal.js
@@ -0,0 +1,27 @@
+function esDiagonalmenteDominante(matriz) {
+    const coeficientes = matriz.coeficientes;
+
+    let esDominante = true;
+
+    // recorre matriz por filas
+    for (let i = 0; i < coeficientes[0].length; i++) {
+        let suma = new Decimal(0);
+        let elementoDiagonal = new Decimal(coeficientes[i][i]).abs();
+
+        for(let j = 0; j < coeficientes.length; j++) {
+            if (i !== j) suma = suma.plus(new Decimal(coeficientes[i][j]).abs());
+        }
+
+        // no es diagonalmente dominante
+        if (suma.greaterThan(elementoDiagonal)) {
+            esDominante = false;
+            break;
+        }
+    }
+
+    if (esDominante) {
+        console.log("es dominante")
+    } else {
+        console.log("no es dominante")
+    }
+}
