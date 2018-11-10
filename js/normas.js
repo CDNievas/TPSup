cb4e54e8cae1ead3ae225509122f20d8ef7dfc2b
julianpebe <julianperezbadon@hotmail.com>
2018-11-10 20:25:08 -0300

    Norma 1 y Norma infinito (estaba dos veces cada funcion). #2

--- js/normas.js
+++ js/normas.js
@@ -30,36 +30,4 @@ function normaUno(matriz) {
     }
 	
     return resultado;
-}function normaInfinito(matriz) {
-	const coeficientes = matriz.coeficientes;
-
-    let resultado = new Decimal(0);
-	
-    for(let i = 0; i < coeficientes[0].length; i++) {
-		let sumaFila = new Decimal(0);
-        
-		for(let j = 0; j < coeficientes.length; j++) {
-			sumaFila = sumaFila.plus(new Decimal(coeficientes[i][j]).abs());
-        }
-		resultado = Decimal.max(resultado, sumaFila);
-    }
-	
-    return resultado;
-}
-
-function normaUno(matriz) {
-	const coeficientes = matriz.coeficientes;
-
-    let resultado = new Decimal(0);
-	
-    for(let j = 0; j < coeficientes[0].length; j++) {
-		let sumaColumna = new Decimal(0);
-        
-		for(let i = 0; i < coeficientes.length; i++) {
-			sumaColumna = sumaColumna.plus(new Decimal(coeficientes[i][j]).abs());
-        }
-		resultado = Decimal.max(resultado, sumaColumna);
-    }
-	
-    return resultado;
 }
